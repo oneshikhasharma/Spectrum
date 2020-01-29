c6d2769afb298d673923454a4a54df6f2471392c
Shikha Sharma <shikha.sharma@actiknow.com>
2020-01-29 11:52:40 +0530

    Initial Commit

---
+++ spectrum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spectrum
+//
+//  Created by Shikha Sharma on 1/29/20.
+//  Copyright © 2020 Shikha Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
